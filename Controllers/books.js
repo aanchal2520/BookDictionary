fb25179cde6441f2a42dfa8356b6a15ac0ec67ef
aanchal2520 <aanchal2520@gmail.com>
2021-08-04 12:29:42 +0530

    Changes made to update book api

--- Controllers/books.js
+++ Controllers/books.js
@@ -19,33 +19,36 @@ exports.getBooks = (req, res) => {
 //update book api -> 
 exports.updateBook = (req, res) => {
 
-    const{ book_name, updated_book_name, author_name, description, image_url, copies_available } = req.body;
+    const{ book_id, book_name, author_name, description, image_url, copies_available } = req.body;
 
-    db.query('Select book_id from books where book_name = ' + mysql.escape(book_name), (err0, res0) => {
-        if(!err0) {
-            if(res0.length === 0) {
-                return res.status(404).json({ message: 'Book does not exist' });
-            } else {
-                db.query('Update books set book_name = ' + mysql.escape(updated_book_name) + 
+    db.query('Update books set book_name = ' + mysql.escape(book_name) + 
                 ', author_name = ' + mysql.escape(author_name) + 
                 ', description = ' + mysql.escape(description) + 
                 ', image_url = ' + mysql.escape(image_url) + 
                 ', copies_available = ' + mysql.escape(copies_available) +
-                ' where book_id = ' + mysql.escape(res0[0].book_id), (err1, res1) => {
+                ' where book_id = ' + mysql.escape(book_id), (err1, res1) => {
                     if(!err1) {
                         return res.status(200).json({ message: 'Book successfully updated' });
                     } else {
                         console.log(err1);
                         return res.status(500).json({ message: 'Internal Server Error' });
                     }
-                })
-            }
-        } else {
-            console.log(err0);
-            return res.stauts(500).json({ message: 'Internal Server Error' });
-        }
-    })
+            })
 }
 
+//delete book api -> 
+// exports.deleteBook = (req, res) => {
+
+//     const { book_id } = req.body;
+
+//     db.query('Delete from books where book_id = ' + mysql.escape(book_id), (err0, res0) => {
+//         if(!err0) {
+//             return res.status(200).json({ message: 'Book successfully deleted' });
+//         } else {
+//             console.log(err0);
+//             return res.status(500).json({ message: 'Internal Server Error' });
+//         }
+//     })
+// }
 
 
